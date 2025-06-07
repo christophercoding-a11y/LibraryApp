98cfbc92534f470745a2d5275b4b23e250b4bf19
christophercodinga11y <christopherjack14@gmail.com>
2025-06-06 20:58:19 -0500

    remove the emojis from the nav

--- react/src/components/Nav.js
+++ react/src/components/Nav.js
@@ -5,10 +5,10 @@ const Nav =()=> {
     return (
         <ul className="nav justify-content-center">
             <li className="nav-item">
-                <Link to="/" className="nav-link">Home🏠</Link>
+                <Link to="/" className="nav-link">Home</Link>
             </li>
             <li className="nav-item">
-                <Link to="/book" className="nav-link">Books📖</Link>
+                <Link to="/book" className="nav-link">Books</Link>
             </li>
         </ul>
     )
