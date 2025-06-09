644e74c0c82bbbb6d709b7df803250ce1169bfe6
christophercodinga11y <christopherjack14@gmail.com>
2025-06-09 17:59:32 -0500

    update the alt text on the authorcard

--- react/src/components/AuthorCard.js
+++ react/src/components/AuthorCard.js
@@ -4,7 +4,7 @@ const AuthorCard =(props)=> {
     return (
         <div className="col">
             <div className="card author-card h-100">
-                <img src={`/images/${props.img_url}`}alt={props.title} className="img-fluid image card-img-top h-100" />
+                <img src={`/images/${props.img_url}`}alt={props.author} className="img-fluid image card-img-top h-100" />
                 <div className="card-body">
                     <h2 className="card-title author-name text-capitalize">{props.author}</h2>
                 </div>
